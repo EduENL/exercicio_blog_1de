580bb76428e18e708698f2bf3ca94e7c51dc0c83
EduENL <eduardonuneslima099@gmail.com>
2023-10-19 14:04:09 -0300

    fase_3_concluida

--- post.php
+++ post.php
@@ -17,8 +17,8 @@
 
 <main id="post-container">
     <div class="content-container">
-        <h1 id="main-title"<?= $post['title'] ?>></h1>
-        <p id="post-description"<?= $post['description'] ?>></p>
+        <h1 id="main-title"><?= $currentPost['title'] ?></h1>
+        <p id="post-description"><?= $currentPost['description'] ?></p>
         <div class="img-container">
             <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
         </div>
@@ -29,13 +29,14 @@
         <h3 id="tags-title">Tags</h3>
         <ul id="tag-list">
             <?php foreach($currentPost['tags'] as $tag): ?>
-                <li><a href="#"<?= $tag ?>></a></li>
+                <li><a href="#"><?= $tag ?></a></li>
             <?php endforeach; ?>
         </ul>
-        <h3 id="tags-title">Tags</h3>
-        <ul id="tag-list">
-            <?php foreach($currentPost['tags'] as $tag): ?>
-                <li><a href="#"<?= $tag ?>></a></li>
+
+        <h3 id="categories-title">Categorias</h3>
+        <ul id="categories-list">
+            <?php foreach($categories as $category): ?>
+                <li><a href="#"><?= $category ?></a></li>
             <?php endforeach; ?>
         </ul>
     </div>
